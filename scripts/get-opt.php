d8a1bc5fcd93ebf708ffbd4a89f393c843fe2ee7
Alexander Afanasyev <alexander.afanasyev@ucla.edu>
2009-05-04 15:28:35 -0700

    Simple BGP parser script Small modification to IPv4 assignment parsing script

--- scripts/get-opt.php
+++ scripts/get-opt.php
@@ -10,22 +10,71 @@ if( !$DB )
 }
 
 //system( "wget ftp://ftp.arin.net/pub/stats/arin/delegated-arin-latest" );
-$date=new DateTime( "2003-01-01" );
-$last=new DateTime( "2004-01-01" );
+if( $argc<6 )
+{
+	print "Usage:\n";
+	print "		php get-opt.php <URL> <RIR> <FROM_DATE> <TO_DATE> <ZIP> <OLD-FORMAT>\n";
+	exit( 1 );
+}
+
+$url=$argv[1];
+$RIR=$argv[2];
+
+$date=new DateTime( $argv[3] );
+$last=new DateTime( $argv[4] );
+
+$bzipped=$argv[5];
+$old_format=$argv[6];
 
 while( $date<$last )
 {
-	$file="ripencc.".$date->format( "Ymd" );
-	print $file."\n";
-	system( "wget ftp://ftp.ripe.net/ripe/stats/old-format/$file 2>/dev/null" );
+	$file=get_file( $date );
 	
 	print ">> ".date( "h:i:s a" )."\n";
-	parse_delegated( $date->format("Y-m-d"),$file,false );
+	parse_delegated( $date->format("Y-m-d"),$file,!$old_format );
 	print "<< ".date( "h:i:s a" )."\n";
 	
 	unlink( "$file" );
 	
 	$date->modify( "1 week" );
+}
+
+function get_file( $date )
+{
+	global $bzipped, $url, $RIR, $old_format;
+
+	if( $old_format=="apnic" )	
+		$file="$RIR-".$date->format( "Y-m-d" );
+	elseif( isset($old_format) )
+		$file="$RIR.".$date->format( "Ymd" );
+	else
+		$file="delegated-$RIR-".$date->format( "Ymd" );
+	
+	print $file."\n";
+	
+	if( $bzipped=="" || $bzipped=="none" )
+	{
+		system( "wget $url/$file 2>/dev/null" );
+	}
+	else if( $bzipped=="gunzip" )
+	{
+		system( "wget $url/$file.gz 2>/dev/null" );
+		system( "$bzipped $file.gz" );
+	}
+	else
+	{
+		system( "wget $url/$file.bz2 2>/dev/null" );
+		system( "$bzipped $file.bz2" );
+	}
+
+	if( !file_exists($file) )
+	{
+		$newdate=new DateTime( $date->format("Y-m-d") );
+		$newdate->modify( "1 day" );
+		return get_file( $newdate );
+	}
+	
+	return $file;
 }
 
 
@@ -48,7 +97,12 @@ function parse_delegated( $on_date, $file, $new_format=true )
 		
 		if( $new_format )
 		{
-			$RIR_date="'".substr($data[5],0,4)."-".substr($data[5],4,2)."-".substr($data[5],6,2)."'";
+			$year=substr($data[5],0,4);
+			$month=substr($data[5],4,2); if( $year!="0000" && $month="00" ) $month="01";
+			$day=substr($data[5],6,2); if( $year!="0000" && $day=="00") $day="01";
+			
+			$RIR_date="'$year-$month-$day'";
+			
 		}
 		else
 		{
